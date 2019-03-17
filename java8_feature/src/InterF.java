ca24dfc4c3035c5eb5760607936505ba8846c189
mahendrakr <mahendrakumar7028@gmail.com>
2019-03-17 22:21:00 +0530

    first commit

---
+++ java8_feature/src/InterF.java
@@ -0,0 +1,21 @@
+
+
+public interface InterF {
+	public void m();
+
+	default void m2() {
+
+	}
+
+	default int m3() {
+		return 0;
+	}
+
+	public static void m4() {
+
+	}
+
+	public static void m5() {
+
+	}
+}
---
+++ java8_feature/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		InterF f = () -> {
+			System.out.println("Hello Lambda");
+		};
+		f.m();
+	}
+
+}
